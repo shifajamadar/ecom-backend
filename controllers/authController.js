f55d89ebcfbac4567685be4880a72121e904e957
shifajamadar <shifajamada4@gmail.com>
2025-07-25 15:42:45 +0530

    Authentication

---
+++ controllers/authController.js
@@ -0,0 +1,102 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "please provide both email and password",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "email already exists",
+      });
+    }
+
+    const hashedpassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      password: hashedpassword,
+    });
+
+    res.status(201).json({
+      message: "registration Done",
+      user,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "please provide both email and password",
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const isvalidpassword = await bcrypt.compare(password, user.password);
+
+    if (!isvalidpassword) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      { email: user.email, role: user.role },
+      process.env.jwt_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 3600000,
+      httpOnly: true,
+    });
+
+    res.json({
+      message: "login done",
+      token,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+};
+
+const logout = (req, res) => {
+  res.clearCookie("jwt");
+
+  res.json({
+    message: "Logout successful",
+  });
+};
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const productRoutes = require("./routes/ProductRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 app.use("/", productRoutes);
+
+app.use("/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URL)
